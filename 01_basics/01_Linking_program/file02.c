faa4327618e9e88e05e117b848ae34dcc2f68546
pkpidurkar <paraspidurkar97@gmail.com>
2024-05-29 01:04:08 +0530

    29May Day01

---
+++ 01_basics/01_Linking_program/file02.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "file02.h"
+
+void function_from_file2() {
+    printf("This is file2.\n");
+}
---
+++ 01_basics/01_Linking_program/file02.h
@@ -0,0 +1,6 @@
+#ifndef FILE02_H
+#define FILE02_H
+
+void function_from_file2();
+
+#endif // FILE02_H
