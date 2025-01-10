4c2738c3d898aca8a847a2cfc279a5105c55bce0
Paras <paraspidurkar97@gmail.com>
2025-01-11 02:56:46 +0530

    Delete 01_basics directory

--- 01_basics/01_Linking_program/file02.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "file02.h"
-
-void function_from_file2() {
-    printf("This is file2.\n");
-}
--- 01_basics/01_Linking_program/file02.h
+++
@@ -1,6 +0,0 @@
-#ifndef FILE02_H
-#define FILE02_H
-
-void function_from_file2();
-
-#endif // FILE02_H
